2f7af18d09f83b05bbcff6ae3f0db041f15642f9
[nome] <davidscattone10@gmail.com>
2023-07-21 13:39:53 +0200

    modifiche

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
@@ -6,34 +6,42 @@ import { HttpClient, HttpParams } from '@angular/common/http';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
   selectedSegment = 'movies';
   mediaList: any[] = [];
   groupedMediaList: any[] = [];
-  searchTerm: string = ''; // Aggiungi il termine di ricerca
-  filteredMediaList: any[] = []; // Aggiungi l'elenco filtrato
+  searchTerm: string = '';
+  filteredMediaList: any[] = [];
 
-  constructor(private http: HttpClient) {
-    this.getRandomMediaList(); // Carica i 4 film casuali di default all'avvio
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getRandomMediaList();
   }
 
   getRandomMediaList() {
-    // Assumi che tu abbia un elenco di film casuali predefiniti (puoi anche ottenere film casuali da un'altra API)
-    const randomMovies = [
-      { Title: 'Film Casuale 1', Poster: 'url-poster-1' },
-      { Title: 'Film Casuale 2', Poster: 'url-poster-2' },
-      { Title: 'Film Casuale 3', Poster: 'url-poster-3' },
-      { Title: 'Film Casuale 4', Poster: 'url-poster-4' },
-      // Puoi aggiungere altri film casuali qui o ridurre il numero a 4
-    ];
+    const apiKey = '893e80b9';
+    const apiUrl = 'http://www.omdbapi.com/';
+    const randomMovieIds = ['tt0111161', 'tt0468569', 'tt1375666', 'tt0137523', 'tt0120737', 'tt0167260', 'tt0133093', 'tt0172495',]; // Lista di ID casuali di film da OMDB
 
-    this.mediaList = randomMovies;
-    this.groupedMediaList = this.chunkArray(this.mediaList, 5);
+    // Effettua una chiamata all'API per ottenere i dati dei film casuali
+    randomMovieIds.forEach(movieId => {
+      let params = new HttpParams();
+      params = params.append('apikey', apiKey);
+      params = params.append('i', movieId); // Utilizza l'ID del film come parametro 'i'
 
-    // Applica il filtro solo se è presente un termine di ricerca
-    if (this.searchTerm) {
-      this.filterMediaList();
-    }
+      this.http.get(apiUrl, { params }).subscribe((response: any) => {
+        if (response) {
+          this.filteredMediaList.push(response); // Aggiungi il film alla lista dei film casuali
+          this.groupedMediaList = this.chunkArray(this.filteredMediaList, 5);
+
+          // Applica il filtro solo se è presente un termine di ricerca
+          if (this.searchTerm) {
+            this.filterMediaList();
+          }
+        }
+      });
+    });
   }
 
   getMediaList() {
@@ -43,35 +51,30 @@ export class Tab1Page {
     let params = new HttpParams();
     params = params.append('apikey', apiKey);
     params = params.append('type', this.selectedSegment);
-    params = params.append('s', this.searchTerm); // Utilizza il termine di ricerca nel parametro 's'
+    params = params.append('s', this.searchTerm);
 
     this.http.get(apiUrl, { params }).subscribe((response: any) => {
       if (response && response.Search) {
         this.mediaList = response.Search;
         this.groupedMediaList = this.chunkArray(this.mediaList, 5);
 
-        // Aggiorna l'elenco filtrato con i risultati della ricerca
         this.filterMediaList();
       }
     });
   }
 
   filterMediaList() {
-    // Trasforma il termine di ricerca in minuscolo per effettuare una ricerca case-insensitive
     const searchTermLower = this.searchTerm.toLowerCase();
 
-    // Se searchTerm è vuoto, mostra i film casuali di default
     if (!this.searchTerm) {
       this.filteredMediaList = this.mediaList;
     } else {
-      // Altrimenti, filtra l'elenco di media in base al termine di ricerca
       this.filteredMediaList = this.mediaList.filter(media => {
         const titleLower = media.Title.toLowerCase();
-        return titleLower.includes(searchTermLower); // Restituisce true se il titolo include il termine di ricerca
+        return titleLower.includes(searchTermLower);
       });
     }
 
-    // Se vuoi anche raggruppare i risultati filtrati in righe di cinque card, puoi farlo qui
     this.groupedMediaList = this.chunkArray(this.filteredMediaList, 5);
   }
 
@@ -87,6 +90,7 @@ export class Tab1Page {
     this.getMediaList();
   }
 }
+
 
 
 
